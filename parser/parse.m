5c20427aedada7e846a26d2e5c85ef785eb27828
sgautam10 <63962527+sgautam10@users.noreply.github.com>
2024-02-24 14:09:22 +0100

    Add files via upload

--- parser/parse.m
+++ parser/parse.m
@@ -19,25 +19,31 @@ end
 
 %%
 
-data = [];
-label = [];
-sound_attended = [];
-sound_masker = [];
+data = zeros([31*4,20,66,8448]);
+label = zeros([31*4,20]);
+sound_attended = strings([31*4,20]);
+sound_masker = strings([31*4,20]);
+d = zeros([20,66,8448]);
 for k =progress(1:numel(l))
     s = load(l(k));
     ica = s.ICAcleaned_data_allsess;
     for j = 1:numel(ica)
-        data = [data;cell2mat(ica{j}.trial)];
-        label = [label;ica{j}.trialid(:,6,:)];
+        for z = 1:20
+            d(z,:,:) = ica{j}.trial{z}(:,5*256+1:38*256);
+        end
+        data(4*(k-1)+j,:,:,:) = d;
+        label(4*(k-1)+j,:) = ica{j}.trialid(:,6,:);
         a = ica{j}.targetfiles;
         m = ica{j}.maskerfiles;
-        sound_attended = [sound_attended;cellfun(@myfun,a)];
-        sound_masker = [sound_masker;cellfun(@myfun,m)];
+        sound_attended(4*(k-1)+j,:) = cellfun(@myfun,a);
+        sound_masker(4*(k-1)+j,:) = cellfun(@myfun,m);
     end
 end
 
-sound_attended = dirpath+sound_attended;
-sound_masker = dirpath + sound_masker
+sound_attended = dirpath+ "\Files_Audio\" + sound_attended;
+sound_masker = dirpath +"\Files_Audio\" + sound_masker;
+sound_attended = convertStringsToChars(sound_attended);
+sound_masker = convertStringsToChars(sound_masker);
 
 
 %%
